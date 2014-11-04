ac8ec3339ac40dd552a657718e565eff219b8b99
Douglas Muth <dmuth@dmuth.org>
2014-11-03 23:30:02 -0500

    Added ping script

---
+++ bin/ping.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Ping a specified host.
+# The results will be reported to (and stored in) Splunk.
+#
+
+TARGET="google.com"
+#NUM_PINGS=3 # Debugging
+NUM_PINGS=10 # Debugging
+
+
+ping -c ${NUM_PINGS} -q ${TARGET}
+
+
+
