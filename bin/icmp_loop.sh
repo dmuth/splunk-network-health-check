597c3f38f129ca9cb7260b81dc944474c5097097
Douglas Muth <dmuth@dmuth.org>
2014-11-04 23:06:26 -0500

    Updated time

--- bin/icmp_loop.sh
+++ bin/icmp_loop.sh
@@ -17,13 +17,29 @@ echo "# Hit ^C to exit."
 echo "# Be sure to re-enable ICMP after existing!"
 echo "#"
 
+#
+# I recommend prime numbers so that the results change slightly for each interval.
+#
+NUM_SECS=3
+#NUM_SECS=13
+#NUM_SECS=79
+
 
 while true
 do
+
 	./icmp_drop.sh
-	sleep 13
+	echo "#"
+	echo "# Sleeping for ${NUM_SECS} seconds..."
+	echo "#"
+	sleep $NUM_SECS
+
 	./icmp_allow.sh
-	sleep 13
+	echo "#"
+	echo "# Sleeping for ${NUM_SECS} seconds..."
+	echo "#"
+	sleep $NUM_SECS
+
 done
 
 
