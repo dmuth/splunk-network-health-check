d9c83ae54fe5ccf7b476a03f1c056a335b47bfc1
Douglas Muth <dmuth@dmuth.org>
2018-07-01 15:54:03 -0400

    Added code to drop/allow DNS packets as well, so as to better simulate a network outage.

--- Network-Monitor/bin/icmp_allow.sh
+++ Network-Monitor/bin/icmp_allow.sh
@@ -7,7 +7,10 @@ set -e # Errors are fatal
 
 
 iptables -D OUTPUT -p icmp -j DROP
+iptables -D OUTPUT -p udp --dport 53 -j DROP
+iptables -D OUTPUT -p tdp --dport 53 -j DROP
+
 echo "#"
-echo "# Now allowing all ICMP traffic"
+echo "# Now allowing all ICMP and DNS traffic"
 echo "#"
 
--- Network-Monitor/bin/icmp_drop.sh
+++ Network-Monitor/bin/icmp_drop.sh
@@ -7,7 +7,10 @@ set -e # Errors are fatal
 
 
 iptables -A OUTPUT -p icmp -j DROP
+iptables -A OUTPUT -p udp --dport 53 -j DROP
+iptables -A OUTPUT -p tdp --dport 53 -j DROP
+
 echo "#"
-echo "# Now dropping all ICMP traffic"
+echo "# Now dropping all ICMP and DNS traffic"
 echo "#"
 
