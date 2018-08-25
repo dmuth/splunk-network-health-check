17c3692e7180927749896aaef58a6b6f237fc124
Douglas Muth <dmuth@dmuth.org>
2018-08-24 22:27:33 -0400

    Fixed typo

--- splunk-app/bin/icmp_allow.sh
+++ splunk-app/bin/icmp_allow.sh
@@ -8,7 +8,7 @@ set -e # Errors are fatal
 
 iptables -D OUTPUT -p icmp -j DROP
 iptables -D OUTPUT -p udp --dport 53 -j DROP
-iptables -D OUTPUT -p tdp --dport 53 -j DROP
+iptables -D OUTPUT -p tcp --dport 53 -j DROP
 
 echo "#"
 echo "# Now allowing all ICMP and DNS traffic"
