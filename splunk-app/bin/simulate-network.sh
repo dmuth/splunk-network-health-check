fc86c3f2cfbd0f7408f989c32d7125f8b81c75cf
Douglas Muth <dmuth@dmuth.org>
2018-09-02 18:01:21 -0400

    Added support for parsing arguments.

--- splunk-app/bin/simulate-network.sh
+++ splunk-app/bin/simulate-network.sh
@@ -10,13 +10,73 @@ LATENCY_DISTRIBUTION="normal"
 LOSS="10"
 TIME=10
 
+function print_syntax() {
+	echo "! "
+	echo "! Syntax: $0 [ --latency num ] [ --jitter num ] [ --distribution normal|pareto|paretonormal ] [ --correlation num ] [ --loss num ] [ --time num ]"
+	echo "! "
+	echo "!	--latency Latency to add to packets (in milliseconds)"
+	echo "!	--jitter Jitter to add to the latency (in milliseconds)"
+	echo "!	--distribution This can be normal, pareto, or paretonormal"
+	echo "!	--correlation Percentage chance of previous packet affecting current packet"
+	echo "!	--loss Percentage of packets lost"
+	echo "!	--time How many seconds to use these settings for"
+	echo "! "
+	exit 1
+}
 
-# --latency
-# --jitter
-# --latency-distribution
-# --latency-correlation
-# --loss
-# --time
+
+function parse_args() {
+
+	while test "$1"
+	do
+
+		ARG=$1
+		ARG_NEXT=$2
+		shift
+
+		if test "$ARG" == "-h" -o "$ARG" == "--help"
+		then
+			print_syntax
+
+		elif test "$ARG" == "--latency"
+		then
+			LATENCY=$ARG_NEXT
+			shift
+
+		elif test "$ARG" == "--jitter"
+		then
+			JITTER=$ARG_NEXT
+			shift
+
+		elif test "$ARG" == "--distribution"
+		then
+			LATENCY_DISTRIBUTION=$ARG_NEXT
+			shift
+
+		elif test "$ARG" == "--correlation"
+		then
+			LATENCY_CORRELATION=$ARG_NEXT
+			shift
+
+		elif test "$ARG" == "--loss"
+		then
+			LOSS=$ARG_NEXT
+			shift
+
+		elif test "$ARG" == "--time"
+		then
+			TIME=$ARG_NEXT
+			shift
+
+		else
+			echo "! Unknown argument: $ARG"
+			print_syntax
+
+		fi
+
+	done
+
+} # End of parse_args()
 
 
 function init() {
@@ -33,6 +93,8 @@ function init() {
 trap init SIGINT SIGTERM
 
 init
+
+parse_args $@
 
 CMD="tc qdisc replace dev eth0 root netem delay ${LATENCY}ms ${JITTER}ms ${LATENCY_CORRELATION}% distribution ${LATENCY_DISTRIBUTION} loss ${LOSS}%"
 
