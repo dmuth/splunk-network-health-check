144c0f43840e996828fec7af14f89b9fc0afee91
Douglas Muth <dmuth@dmuth.org>
2018-09-02 19:11:44 -0400

    Added simulation for Amtrak's Baltimore tunnel.

---
+++ splunk-app/bin/simulate-amtrak-baltimore-tunnel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Simulate a tunnel by baltimore on Amtrak.
+# This included like a 3 minute blackout of total packet less
+#
+
+# Errors are fatal
+set -e
+
+#
+# Change to the directory of this script
+#
+pushd $(dirname $0) > /dev/null
+
+
+TIME_SURFACE=120
+#TIME_SURFACE=5 # Debugging
+TIME_TUNNEL=240
+#TIME_TUNNEL=10 # Debugging
+
+function surface() {
+
+	echo "# Simulating surface conditions..."
+	./simulate-network.sh --time ${TIME_SURFACE} --latency 200 --jitter 50 --distribution pareto --loss 5
+
+}
+
+
+function tunnel() {
+	echo "# Simulating the tunnel conditions..."
+	./simulate-network.sh --time ${TIME_TUNNEL} --loss 100
+}
+
+surface
+tunnel
+surface
+
+
+
