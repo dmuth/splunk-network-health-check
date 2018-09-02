fb6afe75345159ba3a1ea620ca87c8df1838442d
Douglas Muth <dmuth@dmuth.org>
2018-09-02 19:26:23 -0400

    Added packet loss correlation support.

--- splunk-app/bin/simulate-network.sh
+++ splunk-app/bin/simulate-network.sh
@@ -8,17 +8,19 @@ JITTER="10"
 LATENCY_CORRELATION="0"
 LATENCY_DISTRIBUTION="normal"
 LOSS="10"
+LOSS_CORRELATION=25
 TIME=10
 
 function print_syntax() {
 	echo "! "
-	echo "! Syntax: $0 [ --latency num ] [ --jitter num ] [ --distribution normal|pareto|paretonormal ] [ --correlation num ] [ --loss num ] [ --time num ]"
+	echo "! Syntax: $0 [ --latency num ] [ --jitter num ] [ --distribution normal|pareto|paretonormal ] [ --correlation num ] [ --loss num ] [ --loss-correlation num ] [ --time num ]"
 	echo "! "
 	echo "!	--latency Latency to add to packets (in milliseconds)"
 	echo "!	--jitter Jitter to add to the latency (in milliseconds)"
 	echo "!	--distribution This can be normal, pareto, or paretonormal"
 	echo "!	--correlation Percentage chance of previous packet affecting current packet"
 	echo "!	--loss Percentage of packets lost"
+	echo "!	--loss-correlation percent to correlate with drop decision for previous packet."
 	echo "!	--time How many seconds to use these settings for"
 	echo "! "
 	exit 1
@@ -61,6 +63,11 @@ function parse_args() {
 		elif test "$ARG" == "--loss"
 		then
 			LOSS=$ARG_NEXT
+			shift
+
+		elif test "$ARG" == "--loss-correlation"
+		then
+			LOSS_CORRELATION=$ARG_NEXT
 			shift
 
 		elif test "$ARG" == "--time"
@@ -107,6 +114,7 @@ echo "# Latency Jitter: ${JITTER} ms"
 echo "# Latency Correlation: ${LATENCY_CORRELATION}%"
 echo "# Latency Distribution: ${LATENCY_DISTRIBUTION}"
 echo "# Packet Loss: ${LOSS}%"
+echo "# Packet Loss Correlation: ${LOSS_CORRELATION}%"
 echo "# Time: ${TIME} seconds"
 echo "# "
 echo "# Command: ${CMD}"
