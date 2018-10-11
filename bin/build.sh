5aae982e4302386cce846ac8a4275365cd96aebd
Douglas Muth <dmuth@dmuth.org>
2018-10-11 19:39:54 -0400

    Added support for Docker build stages

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,26 @@
 # Errors are fatal
 set -e
 
+TARGET=""
+
+function print_syntax() {
+	echo "# "
+	echo "# Syntax: $0 [ dockerfile_stage ]"
+	echo "# "
+	exit 1
+}
+
+if test "$1" == "-h" -o "$1" == "--help"
+then
+	print_syntax
+fi
+
+if test "$1"
+then
+	TARGET="--target $1"
+fi
+
+
 #
 # Switch to the directory where this script lives and go up a directory
 #
@@ -12,6 +32,6 @@ cd ..
 echo "# "
 echo "# Building container..."
 echo "# "
-docker build . -t splunk-network-health-check 
+docker build ${TARGET} -t splunk-network-health-check .
 
 
