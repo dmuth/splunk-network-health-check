ac47abdf3ae26ea82ddce80f0970e58471cfc816
Douglas Muth <dmuth@dmuth.org>
2019-06-01 23:31:33 -0400

    No longer needing to tag, here.

--- bin/dev.sh
+++ bin/dev.sh
@@ -12,10 +12,5 @@ cd ..
 
 ./bin/build.sh
 
-echo "# "
-echo "# Tagging container..."
-echo "# "
-docker tag splunk-network-health-check dmuth1/splunk-network-health-check
-
 SPLUNK_DEVEL=1 SPLUNK_PASSWORD=${SPLUNK_PASSWORD:-password1} ./go.sh
 
